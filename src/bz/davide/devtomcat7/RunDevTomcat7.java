ed396a15227cce3b18318622ed736b71163bba85
davide <d@vide.bz>
2014-03-30 11:45:07 +0200

    RunDevTomcat7AndGwtDevMode

--- src/bz/davide/devtomcat7/RunDevTomcat7.java
+++ src/bz/davide/devtomcat7/RunDevTomcat7.java
@@ -1,3 +1,21 @@
+/*
+RunDevTomcat7 - Run embedded tomcat 7 for development - http://www.davide.bz
+
+Copyright (C) 2013-2014 Davide Montesin <d@vide.bz> - Bolzano/Bozen - Italy
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>
+*/
 
 package bz.davide.devtomcat7;
 
@@ -40,7 +58,7 @@ public class RunDevTomcat7
                          + " associated to path "
                          + warFile.getPath());
 
-      int port = 9999;
+      int port = 8888;
       Tomcat tomcat = new Tomcat();
       tomcat.setPort(port);
 
--- src/bz/davide/devtomcat7/RunDevTomcat7AndGwtDevMode.java
+++ src/bz/davide/devtomcat7/RunDevTomcat7AndGwtDevMode.java
@@ -1,10 +1,57 @@
+/*
+RunDevTomcat7 - Run embedded tomcat 7 for development - http://www.davide.bz
+
+Copyright (C) 2013-2014 Davide Montesin <d@vide.bz> - Bolzano/Bozen - Italy
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>
+*/
 
 package bz.davide.devtomcat7;
 
+import java.io.IOException;
+import javax.servlet.ServletException;
+import org.apache.catalina.LifecycleException;
+
 public class RunDevTomcat7AndGwtDevMode
 {
-   public static void main(String[] args)
+   public static void main(final String[] args) throws ServletException, LifecycleException, IOException
    {
-      com.google.gwt.dev.DevMode.main(args);
+      // Search for -war parameter
+      String war = "war";
+      for (int i = 0; i < args.length; i++)
+      {
+         String arg = args[i];
+         if (arg.equals("-war"))
+         {
+            war = args[i + 1];
+         }
+      }
+
+      Thread t = new Thread(new Runnable()
+      {
+         @Override
+         public void run()
+         {
+            System.out.println("Starting dev mode");
+            com.google.gwt.dev.DevMode.main(args);
+            System.out.println("Dev mode stopped");
+
+         }
+      });
+      t.start();
+
+      RunDevTomcat7.main(new String[] { "/", war });
+      System.exit(0);
    }
 }

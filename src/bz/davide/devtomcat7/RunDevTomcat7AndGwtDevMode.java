b6cd0d2df874743e043c4d12ccdf865e6287ce4f
davide <d@vide.bz>
2014-03-30 11:10:18 +0200

    run tomcat and gwt

---
+++ src/bz/davide/devtomcat7/RunDevTomcat7AndGwtDevMode.java
@@ -0,0 +1,10 @@
+
+package bz.davide.devtomcat7;
+
+public class RunDevTomcat7AndGwtDevMode
+{
+   public static void main(String[] args)
+   {
+      com.google.gwt.dev.DevMode.main(args);
+   }
+}
